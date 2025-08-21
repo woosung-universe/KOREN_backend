6b73f8165a67c99eb9560b8d0133f879aa2dc120
Jeong Yeonkyung <edaily0129@gmail.com>
2025-08-22 02:03:59 +0900

    chore: DB 연결 및 dotenv 불러오기

---
+++ app/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+import os
+
+# PostgreSQL 연결 URL
+DATABASE_URL = os.getenv(
+    "DATABASE_URL"
+)
+
+# 엔진 생성
+engine = create_engine(DATABASE_URL, echo=True)
+
+# 세션 로컬 클래스
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# 의존성 (Dependency)
+def get_db() -> Session:
+  db = SessionLocal()
+  try:
+    yield db
+  finally:
+    db.close()
